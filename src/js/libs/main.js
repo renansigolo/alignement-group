540c4299f6e1d2231911f76c275c95d9215f2dde
Renan Sigolo <renan.sigolo@gmail.com>
2020-06-13 17:55:37 +1000

    build: add more images

--- src/js/libs/main.js
+++ src/js/libs/main.js
@@ -1,68 +1,71 @@
-(function($) {
+;(function ($) {
+  'use strict'
 
-	'use strict';
-
-	// bootstrap dropdown hover
+  // bootstrap dropdown hover
 
   // loader
-  var loader = function() {
-    setTimeout(function() { 
-      if($('#loader').length > 0) {
-        $('#loader').removeClass('show');
+  var loader = function () {
+    setTimeout(function () {
+      if ($('#loader').length > 0) {
+        $('#loader').removeClass('show')
       }
-    }, 1);
-  };
-  loader();
+    }, 1)
+  }
+  loader()
 
-	
-	$('nav .dropdown').hover(function(){
-		var $this = $(this);
-		$this.addClass('show');
-		$this.find('> a').attr('aria-expanded', true);
-		$this.find('.dropdown-menu').addClass('show');
-	}, function(){
-		var $this = $(this);
-			$this.removeClass('show');
-			$this.find('> a').attr('aria-expanded', false);
-			$this.find('.dropdown-menu').removeClass('show');
-	});
+  $('nav .dropdown').hover(
+    function () {
+      var $this = $(this)
+      $this.addClass('show')
+      $this.find('> a').attr('aria-expanded', true)
+      $this.find('.dropdown-menu').addClass('show')
+    },
+    function () {
+      var $this = $(this)
+      $this.removeClass('show')
+      $this.find('> a').attr('aria-expanded', false)
+      $this.find('.dropdown-menu').removeClass('show')
+    }
+  )
 
+  $('#dropdown04').on('show.bs.dropdown', function () {
+    console.log('show')
+  })
 
-	$('#dropdown04').on('show.bs.dropdown', function () {
-	  console.log('show');
-	});
-
-	// home slider
-	$('.home-slider').owlCarousel({
-    loop:true,
-    autoplay: true,
-    margin:10,
+  // home slider
+  $('.home-slider').owlCarousel({
+    loop: true,
+    autoplay: false,
+    margin: 10,
     animateOut: 'fadeOut',
     animateIn: 'fadeIn',
-    nav:true,
+    nav: true,
     autoplayHoverPause: true,
     items: 1,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
+    navText: [
+      "<span class='ion-chevron-left'></span>",
+      "<span class='ion-chevron-right'></span>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
       },
-      600:{
-        items:1,
-        nav:false
+      600: {
+        items: 1,
+        nav: false,
       },
-      1000:{
-        items:1,
-        nav:true
-      }
-    }
-	});
+      1000: {
+        items: 1,
+        nav: true,
+      },
+    },
+  })
 
-	// owl carousel
-	var majorCarousel = $('.js-carousel-1');
-	majorCarousel.owlCarousel({
-    loop:true,
+  // owl carousel
+  var majorCarousel = $('.js-carousel-1')
+  majorCarousel.owlCarousel({
+    loop: true,
     autoplay: true,
     stagePadding: 7,
     margin: 20,
@@ -71,28 +74,31 @@
     nav: true,
     autoplayHoverPause: true,
     items: 3,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
+    navText: [
+      "<span class='ion-chevron-left'></span>",
+      "<span class='ion-chevron-right'></span>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
       },
-      600:{
-        items:2,
-        nav:false
+      600: {
+        items: 2,
+        nav: false,
       },
-      1000:{
-        items:3,
-        nav:true,
-        loop:false
-      }
-  	}
-	});
+      1000: {
+        items: 3,
+        nav: true,
+        loop: false,
+      },
+    },
+  })
 
-	// owl carousel
-	var major2Carousel = $('.js-carousel-2');
-	major2Carousel.owlCarousel({
-    loop:true,
+  // owl carousel
+  var major2Carousel = $('.js-carousel-2')
+  major2Carousel.owlCarousel({
+    loop: true,
     autoplay: true,
     stagePadding: 7,
     margin: 20,
@@ -101,61 +107,60 @@
     nav: true,
     autoplayHoverPause: true,
     items: 4,
-    navText : ["<span class='ion-chevron-left'></span>","<span class='ion-chevron-right'></span>"],
-    responsive:{
-      0:{
-        items:1,
-        nav:false
+    navText: [
+      "<span class='ion-chevron-left'></span>",
+      "<span class='ion-chevron-right'></span>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
       },
-      600:{
-        items:3,
-        nav:false
+      600: {
+        items: 3,
+        nav: false,
       },
-      1000:{
-        items:4,
-        nav:true,
-        loop:false
-      }
-  	}
-	});
+      1000: {
+        items: 4,
+        nav: true,
+        loop: false,
+      },
+    },
+  })
 
+  var contentWayPoint = function () {
+    var i = 0
+    $('.element-animate').waypoint(
+      function (direction) {
+        if (
+          direction === 'down' &&
+          !$(this.element).hasClass('element-animated')
+        ) {
+          i++
 
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.element-animate').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('element-animated') ) {
-				
-				i++;
-
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-
-					$('body .element-animate.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn element-animated');
-							} else if ( effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft element-animated');
-							} else if ( effect === 'fadeInRight') {
-								el.addClass('fadeInRight element-animated');
-							} else {
-								el.addClass('fadeInUp element-animated');
-							}
-							el.removeClass('item-animate');
-						},  k * 100);
-					});
-					
-				}, 100);
-				
-			}
-
-		} , { offset: '95%' } );
-	};
-	contentWayPoint();
-
-
-
-})(jQuery);
+          $(this.element).addClass('item-animate')
+          setTimeout(function () {
+            $('body .element-animate.item-animate').each(function (k) {
+              var el = $(this)
+              setTimeout(function () {
+                var effect = el.data('animate-effect')
+                if (effect === 'fadeIn') {
+                  el.addClass('fadeIn element-animated')
+                } else if (effect === 'fadeInLeft') {
+                  el.addClass('fadeInLeft element-animated')
+                } else if (effect === 'fadeInRight') {
+                  el.addClass('fadeInRight element-animated')
+                } else {
+                  el.addClass('fadeInUp element-animated')
+                }
+                el.removeClass('item-animate')
+              }, k * 100)
+            })
+          }, 100)
+        }
+      },
+      { offset: '95%' }
+    )
+  }
+  contentWayPoint()
+})(jQuery)
